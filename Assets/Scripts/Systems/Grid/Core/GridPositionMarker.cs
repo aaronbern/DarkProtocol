c19b5e992d6bed19bce6b7058793daa5557e7f21
Aaron.Bernard <aaronber@pdx.edu>
2025-05-11 23:24:17 -0700

    progress on grid structure, need to refactor into smaller components. GridManager into it's parts.

---
+++ Assets/Scripts/Systems/Grid/Core/GridPositionMarker.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace DarkProtocol.Grid
+{
+    /// <summary>
+    /// Simple component to store grid position data on interaction colliders
+    /// </summary>
+    public class GridPositionMarker : MonoBehaviour
+    {
+        public Vector2Int GridPosition;
+    }
+}
